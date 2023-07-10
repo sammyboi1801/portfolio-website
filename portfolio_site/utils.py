fabf3cff67562bb13398a179d541f767f64cace6
Sam Selvaraj <80597420+sammyboi1801@users.noreply.github.com>
2023-07-10 22:53:37 +0530

    Update utils.py

--- portfolio_site/utils.py
+++ portfolio_site/utils.py
@@ -5,7 +5,7 @@ def send_email_from_client(email, message):
     subject = "Message by " + str(email)
     message = message
     from_email = settings.EMAIL_HOST_USER
-    print(from_email)
+    # print(from_email)
     recipient_list = ["samselvaraj1801@gmail.com"]
     send_mail(subject,message, from_email, recipient_list)
 
@@ -13,6 +13,6 @@ def send_email_to_client(email):
     subject = "Here you go! :)"
     message = "Here's my CV: https://drive.google.com/file/d/1FV_3fG6ouPOQbVN5WfGrpQu7kUVuBvu-/view?usp=sharing"
     from_email = settings.EMAIL_HOST_USER
-    print(from_email)
+    # print(from_email)
     recipient_list = [email]
     send_mail(subject, message, from_email, recipient_list)
