c5987b9b22dfbef05aaf085d15835a74bfaa864e
Sam Selvaraj <80597420+sammyboi1801@users.noreply.github.com>
2023-07-10 22:07:18 +0530

    Update settings.py

--- portfolio_site/settings.py
+++ portfolio_site/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-y-v#y#7^^xeck5*6z347q&_f&o@rl9cczqyh*r!d=nccq*5xi@'
+SECRET_KEY = 'os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -140,12 +140,8 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-# EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-# EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-# EMAIL_HOST_USER = 'sam.selva1801@gmail.com'
-# EMAIL_HOST_PASSWORD = 'lqyodinwahujwjoi'
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 # print(EMAIL_HOST_USER, EMAIL_HOST_PASSWORD)
 
 
