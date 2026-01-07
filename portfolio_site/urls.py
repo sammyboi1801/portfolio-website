386ebf82d227d023563a4b80831d1ec66c4042f8
Sam Selvaraj <80597420+sammyboi1801@users.noreply.github.com>
2026-01-07 13:06:04 -0500

    Add API route for kai_chat view

--- portfolio_site/urls.py
+++ portfolio_site/urls.py
@@ -22,7 +22,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.home)
+    path('',views.home),
+    path('api/kai-chat/', views.kai_chat, name='kai_chat')
 ]
 # urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 # urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
